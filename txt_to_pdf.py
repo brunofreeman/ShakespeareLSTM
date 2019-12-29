207e133041342c75c9281c24af303a57eccd6bc7
Bruno Freeman <bfreeman@caltech.edu>
2019-12-29 16:49:38 -0500

    difference between import calls and running alone

--- txt_to_pdf.py
+++ txt_to_pdf.py
@@ -21,12 +21,12 @@ def txt_to_pdf(file_path):
     for line in text:
         pdf.cell(ln=1, h=5.0, align="L", w=0, txt=line, border=0)
     pdf.output(os.path.join(".", os.path.join(ROOT, OUTPUT_DIR_1, OUTPUT_DIR_2, file_name + ".pdf")), "F")
-    
 
-if len(sys.argv) != 2:
-    print("Provide path to text file relative to current path as program argument")
-    sys.exit()
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Provide path to text file relative to current path as program argument")
+        sys.exit()
 
-file_to_convert = sys.argv[1]
+    file_to_convert = sys.argv[1]
 
-txt_to_pdf(os.path.join(ROOT, file_to_convert))
+    txt_to_pdf(os.path.join(ROOT, file_to_convert))
